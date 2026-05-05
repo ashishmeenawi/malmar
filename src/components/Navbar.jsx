5adac2913ac61ceb583c359ac24c59bcf8905714
ashishmeenawi <ashishmeenawi@gmail.com>
2026-05-06 02:13:07 +0530

    fix: navbar font issue + gsap fixes + mobile layout + Inner Footer Menu size  Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
